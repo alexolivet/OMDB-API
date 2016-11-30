6acb3349d3a559586da3f326e63c1a33e610fcc8
alexolivet <alexolivet@yahoo.com>
2016-11-30 12:28:05 +0100

    adding timeout value

--- js/main.js
+++ js/main.js
@@ -1,59 +1,66 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$("#movie-title").on('keyup',function(){
-	//Get data from input box
-	var movieTitle = $('#movie-title').val();
-	if (movieTitle.length === 1) {
-		$("#result").html("Please enter at least 2 characters");
-	} 
-	else{
+    //varibale to delay the onkeyup
+    var timer;
 
-		$("#result").html("");
-	//Build our URI with the movie title
-	var sURL = "http://www.omdbapi.com/?s=" + movieTitle + "&plot=full&r=json";
+    $("#movie-title").on('keyup', function() {
+        //Get data from input box
+        var movieTitle = $('#movie-title').val();
+        if (movieTitle.length === 1) {
+        	$("#result").html("Please enter at least 2 characters");
+        } else {
 
-	//Grab our container and assign it to variable for later use
-	var container = $('#container');
+        	$("#result").html("");
+            //Build our URI with the movie title
+            var sURL = "http://www.omdbapi.com/?s=" + movieTitle + "&plot=full&r=json";
 
-	//if
+            //Grab our container and assign it to variable for later use
+            var container = $('#container');
 
-	$.ajax({
-		method: 'GET',
-		url: sURL,
-		success: function(results){
-			//console.log("Done: ", results); // leave this for test purpose
-			var movies = results.Search;
-			for(var i = 0; i <= movies.length-1; i++){
-				container.append('<tr><td> <img src=' + movies[i].Poster + '/> </td>' +
-					'<td>' + movies[i].Title + '</td>' +
-					'<td>' + movies[i].Type + '</td>' +
-					'<td> '+ movies[i].imdbID +'</td>' +
-					'<td>' + movies[i].Year + '</td></tr>');
+            //timeout starts here
+            //before the search starts
+            clearTimeout(timer); //clear any running timeout on key up
+            timer = setTimeout(function() {
 
-			}
+            	$.ajax({
+            		method: 'GET',
+            		url: sURL,
+            		success: function(results) {
+                        //console.log("Done: ", results); // leave this for test purpose
+                        var movies = results.Search;
+                        for (var i = 0; i <= movies.length - 1; i++) {
+                        	container.append('<tr><td> <img src=' + movies[i].Poster + '/> </td>' +
+                        		'<td>' + movies[i].Title + '</td>' +
+                        		'<td>' + movies[i].Type + '</td>' +
+                        		'<td> ' + movies[i].imdbID + '</td>' +
+                        		'<td>' + movies[i].Year + '</td></tr>');
 
-			$('#do-search').hide(); // on success response, hide search button
-			$('#do-search-title').hide(); // on success response, hide search button
-			$('#movie-title').hide(); // on success response, hide search button
-			$('#PageRefresh').show(); // on success response, display new search button
-			$('#container').show(); // on success response, display results in table
+                        }
 
-		},
+                        $('#do-search').hide(); // on success response, hide search button
+                        $('#do-search-title').hide(); // on success response, hide search button
+                        $('#movie-title').hide(); // on success response, hide search button
+                        $('#PageRefresh').show(); // on success response, display new search button
+                        $('#container').show(); // on success response, display results in table
 
-		error: function(error){
-			console.error('@ERROR', error);
-		}
-	});
+                      },
+
+                      error: function(error) {
+                      	console.error('@ERROR', error);
+                      }
+                    });
+                //timeout stops here
+                //this is the place the change the timeout value
+              }, 1000);
 };
 
 });
 
-//refresh the page
-//this is linked to the new search button
-$('#PageRefresh').click(function() {
-	location.reload();
-});
+    //refresh the page
+    //this is linked to the new search button
+    $('#PageRefresh').click(function() {
+    	location.reload();
+    });
 
 
-});//End of document.ready
-
+}); //End of document.ready
